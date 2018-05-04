dc01658ed7e8320ccf0653e9d535e644ba788b97
Jess Bracht <jbrachtie@gmail.com>
2018-05-04 16:23:22 -0400

    add read route

--- notably/app/routes/note_routes.js
+++ notably/app/routes/note_routes.js
@@ -1,7 +1,26 @@
+var ObjectID = require('mongodb').ObjectID;
+
 module.exports = function(app, db) {
+	app.get('/notes/:id', (req, res) => {
+		const details = { '_id': new ObjectID(req.params.id) };
+		db.collection('notes').findOne(details, (err, item) => {
+			if(err) {
+				res.send({ 'error': 'an error has occurred' });
+			} else {
+				res.send(item);
+			}
+		});
+	});
+
 	app.post('/notes', (req, res) => {
 		// create note:
-		console.log(req.body);
-		res.send("A note was created!");
+		const note = { text: req.body.body, title: req.body.title };
+		db.collection('notes').insert(note, (err, result) => {
+			if(err) {
+				res.send({ 'error': 'an error has occurred' });
+			} else {
+				res.send(result.ops[0]);
+			}
+		});
 	});
 }
--- notably/server.js
+++ notably/server.js
@@ -2,16 +2,26 @@
 const express = require('express');
 const MongoClient = require('mongodb').MongoClient;
 const bodyParser = require('body-parser');
+const db = require('./config/db');
 
 const app = express();
 const port = 8000;
 
 app.use(bodyParser.urlencoded({ extended: true }));
 
-// import routes for use:
-require('./app/routes')(app, {});
 
-// tell server to listen for HTTP requests:
-app.listen(port, () => {
-	console.log('We are live on port ' + port);
+// tell server to listen for HTTP requests and set up db connection:
+MongoClient.connect(db.url, (err, database) => {
+	if(err) {
+		return console.log(err);
+	}
+
+	var db = database.db("jesstest");
+
+	// import routes for use:
+	require('./app/routes')(app, db);
+
+	app.listen(port, () => {
+		console.log('We are live on port ' + port);
+	});
 });
